0ffa7178fbe2019953a9a3dacf03a8cc7d9b8e31
yogesh <117835365+yogeshpenumur@users.noreply.github.com>
2025-05-21 22:55:20 +0530

    stage1 commit

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull yogeshpenumur/simple-python-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 yogeshpenumur/simple-python-app
--- stop_container.sh
+++ stop_container.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-
-# Stop the running container (if any)
-echo "Hi"
+containerid=`'docker ps | awk -F " " '{print $1}'`
+docker rm -f $containerid
