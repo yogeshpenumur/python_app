8aeeb9632510f4d1b8e28258ea020b35eeabaa73
yogesh <117835365+yogeshpenumur@users.noreply.github.com>
2025-05-22 09:48:53 +0530

    web

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hellodockerworld!'
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(host='0.0.0.0', port=5000)
